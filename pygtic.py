71e38b7df321cef9a108d7d7400f7f22b3273cba
B Spinks <beauspinks@gmail.com>
2020-06-12 19:48:27 +0100

    Fixed background colour changing issue

--- pygtic.py
+++ pygtic.py
@@ -20,13 +20,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-global colour
-colour = BLACK
+colour = WHITE
 click_loop = 0
 click = False
+b = [' ' for _ in range(10)]
 screenWidth = 900
 screenHeight = 900
-b = [' ' for x in range(10)]
 win = pygame.display.set_mode((screenWidth, screenHeight))
 
 
@@ -35,8 +34,7 @@ class Tiles(object):
     o = pygame.image.load('images/o_blue.png')
     grid = pygame.image.load('images/bg_box.png')
 
-    def __init__(self, colour, rect, clicked=False):
-        self.colour = colour
+    def __init__(self, rect, clicked=False):
         self.rect = rect
         self.clicked = clicked
 
@@ -44,58 +42,14 @@ class Tiles(object):
         win.blit(self.grid, self.rect)
 
     def draw_x(self):
-        win.blit(self.x, self.rect)
+        if self.clicked is False:
+            win.blit(self.x, self.rect)
         self.clicked = True
 
     def draw_o(self):
-        win.blit(self.o, self.rect)
+        if self.clicked is False:
+            win.blit(self.o, self.rect)
         self.clicked = True
-
-
-class Button:
-
-    def __init__(self, text, x, y):
-
-        self.text = text
-        self.width = 100
-        self.height = 50
-
-        self.image_normal = pygame.Surface((self.width, self.height))
-        self.image_normal.fill(BLUE)
-
-        self.image_hovered = pygame.Surface((self.width, self.height))
-        self.image_hovered.fill(GREEN)
-
-        self.image = self.image_normal
-        self.rect = self.image.get_rect()
-
-        text_image = font.render(text, True, WHITE)
-        text_rect = text_image.get_rect(center=self.rect.center)
-
-        self.image_normal.blit(text_image, text_rect)
-        self.image_hovered.blit(text_image, text_rect)
-
-        # you can't use it before `blit`
-        self.rect.topleft = (x, y)
-
-        self.hovered = False
-
-    def update(self):
-
-        if self.hovered:
-            self.image = self.image_hovered
-        else:
-            self.image = self.image_normal
-
-    def draw(self, surface):
-
-        surface.blit(self.image, self.rect)
-
-    def handle_event(self, event):
-
-        if event.type == pygame.MOUSEMOTION:
-            self.hovered = self.rect.collidepoint(event.pos)
-            print('hovered')
 
 
 def draw_text(t, f, c, s, x, y):
@@ -219,54 +173,62 @@ def is_winner(bo, le):
             (bo[3] == le and bo[5] == le and bo[7] == le))
 
 
-def draw_game():
-    T1.draw_grid()
-    T2.draw_grid()
-    T3.draw_grid()
-    T4.draw_grid()
-    T5.draw_grid()
-    T6.draw_grid()
-    T7.draw_grid()
-    T8.draw_grid()
-    T9.draw_grid()
+# def draw_game():
+#     T1.draw_grid()
+#     T2.draw_grid()
+#     T3.draw_grid()
+#     T4.draw_grid()
+#     T5.draw_grid()
+#     T6.draw_grid()
+#     T7.draw_grid()
+#     T8.draw_grid()
+#     T9.draw_grid()
 
-
-T1 = Tiles((0, 0, 0), (180, 180))
-T2 = Tiles((0, 0, 0), (180, 360))
-T3 = Tiles((0, 0, 0), (180, 540))
-T4 = Tiles((0, 0, 0), (360, 180))
-T5 = Tiles((0, 0, 0), (360, 360))
-T6 = Tiles((0, 0, 0), (360, 540))
-T7 = Tiles((0, 0, 0), (540, 180))
-T8 = Tiles((0, 0, 0), (540, 360))
-T9 = Tiles((0, 0, 0), (540, 540))
+T1 = Tiles((180, 180))
+T2 = Tiles((180, 360))
+T3 = Tiles((180, 540))
+T4 = Tiles((360, 180))
+T5 = Tiles((360, 360))
+T6 = Tiles((360, 540))
+T7 = Tiles((540, 180))
+T8 = Tiles((540, 360))
+T9 = Tiles((540, 540))
 
 
 def main_menu():
     global click
     global colour
 
-    while True:
+    btn_1 = pygame.Rect(100, 150, 100, 40)
+    btn_2 = pygame.Rect(250, 150, 100, 40)
+    btn_3 = pygame.Rect(400, 150, 100, 40)
 
+    def draw():
         win.fill(colour)
-        if colour == WHITE:
-            draw_text('Main Menu', h1_font, BLACK, win, 20, 20)
-        else:
+
+        pygame.draw.rect(win, (0, 255, 0), btn_1)
+        pygame.draw.rect(win, (255, 255, 0), btn_2)
+        pygame.draw.rect(win, (255, 0, 255), btn_3)
+
+        if colour == BLACK:
             draw_text('Main Menu', h1_font, WHITE, win, 20, 20)
+        else:
+            draw_text('Main Menu', h1_font, BLACK, win, 20, 20)
 
-        mx, my = pygame.mouse.get_pos()
+        draw_text('2 player', font, BLACK, win, 100, 150)
+        draw_text('vs computer', font, BLACK, win, 250, 150)
+        draw_text('options menu', font, BLACK, win, 400, 150)
 
-        btn_1 = pygame.Rect(100, 150, 100, 40)
-        btn_2 = pygame.Rect(250, 150, 100, 40)
-        btn_3 = pygame.Rect(400, 150, 100, 40)
+    while True:
+        mmx, mmy = pygame.mouse.get_pos()
 
-        if btn_1.collidepoint(mx, my):
+        if btn_1.collidepoint(mmx, mmy):
             if click:
                 game_vs_player()
-        if btn_2.collidepoint(mx, my):
+        if btn_2.collidepoint(mmx, mmy):
             if click:
                 game_vs_comp()
-        if btn_3.collidepoint(mx, my):
+        if btn_3.collidepoint(mmx, mmy):
             if click:
                 options()
 
@@ -283,31 +245,27 @@ def main_menu():
                 if event.button == 1:
                     click = True
 
-        pygame.draw.rect(win, (0, 255, 0), btn_1)
-        pygame.draw.rect(win, (255, 255, 0), btn_2)
-        pygame.draw.rect(win, (255, 0, 255), btn_3)
-        draw_text('2 player', font, BLACK, win, 100, 150)
-        draw_text('vs computer', font, BLACK, win, 250, 150)
-        draw_text('options menu', font, BLACK, win, 400, 150)
-
+        draw()
         pygame.display.update()
         clock.tick(30)
 
 
 def game_vs_player():
     global colour
-
+    global b
     run = True
+    b = [' ' for _ in range(10)]
     win.fill(colour)
-    b = [' ' for x in range(10)]
-    bg = pygame.image.load('images/grid_640+360.png')
-    draw_text('2 player', h1_font, BLACK, win, 20, 20)
 
-    if colour == BLACK:
-        bg = pygame.image.load('images/grid_640+360_white.png')
-    else:
-        bg = pygame.image.load('images/grid_640+360.png')
-    win.blit(bg, (0, 0))
+    def draw():
+        if colour == BLACK:
+            draw_text('2 player', h1_font, WHITE, win, 20, 20)
+            grid = pygame.image.load('images/grid_640+360_white.png')
+        else:
+            draw_text('2 player', h1_font, BLACK, win, 20, 20)
+            grid = pygame.image.load('images/grid_640+360.png')
+
+        win.blit(grid, (0, 0))
 
     while run:
 
@@ -322,43 +280,44 @@ def game_vs_player():
                 elif event.key == pygame.K_BACKSPACE:
                     run = False
 
+        draw()
         pygame.display.update()
         clock.tick(30)
 
 
 def game_vs_comp():
     global click_loop
-    global b
     global colour
-
+    global b
+    global click
     run = True
+    click = False
     click_loop = 1
-    b = [' ' for x in range(10)]
-    win.fill(colour)
-    draw_text('main game', h1_font, BLACK, win, 20, 20)
+    b = [' ' for _ in range(10)]
 
-    if colour == BLACK:
-        bg = pygame.image.load('images/grid_640+360_white.png')
-    else:
-        bg = pygame.image.load('images/grid_640+360.png')
-    win.blit(bg, (0, 0))
+    tile1 = pygame.Rect(180, 180, 180, 180)
+    tile2 = pygame.Rect(180, 360, 180, 180)
+
+    win.fill(colour)
+
+    def draw():
+        if colour == BLACK:
+            grid = pygame.image.load('images/grid_640+360_white.png')
+            draw_text('comp game', h1_font, WHITE, win, 20, 20)
+        else:
+            grid = pygame.image.load('images/grid_640+360.png')
+            draw_text('comp game', h1_font, BLACK, win, 20, 20)
+
+        win.blit(grid, (0, 0))
 
     while run:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                elif event.key == pygame.K_BACKSPACE:
-                    run = False
+        cgx, cgy = pygame.mouse.get_pos()
 
         if pygame.mouse.get_pressed()[0] and click_loop == 0:
+            print('clicked')
             play_move()
             if b.count(' ') > 1:
+                print('computer moved')
                 m = comp_move()
                 b[m] = 'O'
                 if is_winner(b, 'O'):
@@ -368,91 +327,18 @@ def game_vs_comp():
             else:
                 print('Tie Game!')
             click_loop = 1
+            print(b)
 
-        pygame.display.update()
-        clock.tick(30)
+        # if tile1.collidepoint(cgx, cgy):
+        #     if click:
+        #         print('T1 click')
+        #         T1.draw_x()
+        # if tile2.collidepoint(cgx, cgy):
+        #     if click:
+        #         print('T2 click')
+        #         T2.draw_x()
 
-        if click_loop > 0:
-            click_loop += 1
-        if click_loop > 8:
-            click_loop = 0
-
-
-def options():
-    global colour
-    global click
-
-    run = True
-    win.fill(colour)
-
-    if colour == WHITE:
-        draw_text('Options Menu', font, BLACK, win, 20, 20)
-    else:
-        draw_text('Options Menu', font, WHITE, win, 20, 20)
-
-    if colour == WHITE:
-        draw_text('Pick your background colour!', font, BLACK, win, 80, 80)
-    else:
-        draw_text('Pick your background colour!', font, WHITE, win, 80, 80)
-
-    bt_1 = pygame.Rect(100, 300, 100, 40)
-    bt_2 = pygame.Rect(250, 300, 100, 40)
-    bt_3 = pygame.Rect(400, 300, 100, 40)
-    bt_4 = pygame.Rect(550, 300, 100, 40)
-    bt_5 = pygame.Rect(700, 300, 100, 40)
-    bt_6 = pygame.Rect(100, 450, 100, 40)
-    bt_7 = pygame.Rect(250, 450, 100, 40)
-    bt_8 = pygame.Rect(400, 450, 100, 40)
-    bt_9 = pygame.Rect(550, 450, 100, 40)
-    bt_10 = pygame.Rect(700, 450, 100, 40)
-
-    pygame.draw.rect(win, BLACK, bt_1)
-    pygame.draw.rect(win, WHITE, bt_2)
-    pygame.draw.rect(win, YLW, bt_3)
-    pygame.draw.rect(win, BBY_GRN, bt_4)
-    pygame.draw.rect(win, TEAL, bt_5)
-    pygame.draw.rect(win, GNTL_RED, bt_6)
-    pygame.draw.rect(win, PURPLE, bt_7)
-    pygame.draw.rect(win, RED, bt_8)
-    pygame.draw.rect(win, GREEN, bt_9)
-    pygame.draw.rect(win, BLUE, bt_10)
-
-    mx, my = pygame.mouse.get_pos()
-
-    if bt_1.collidepoint(mx, my):
-        if click:
-            colour = BLACK
-    if bt_2.collidepoint(mx, my):
-        if click:
-            colour = WHITE
-    if bt_3.collidepoint(mx, my):
-        if click:
-            colour = YLW
-    if bt_4.collidepoint(mx, my):
-        if click:
-            colour = BBY_GRN
-    if bt_5.collidepoint(mx, my):
-        if click:
-            colour = TEAL
-    if bt_6.collidepoint(mx, my):
-        if click:
-            colour = GNTL_RED
-    if bt_7.collidepoint(mx, my):
-        if click:
-            colour = PURPLE
-    if bt_8.collidepoint(mx, my):
-        if click:
-            colour = RED
-    if bt_9.collidepoint(mx, my):
-        if click:
-            colour = GREEN
-    if bt_10.collidepoint(mx, my):
-        if click:
-            colour = BLUE
-
-    click = False
-    while run:
-
+        click = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -467,6 +353,114 @@ def options():
                 if event.button == 1:
                     click = True
 
+        draw()
+        pygame.display.update()
+        clock.tick(30)
+
+        if click_loop >= 1:
+            click_loop += 1
+        if click_loop > 8:
+            click_loop = 0
+
+
+def options():
+    global click
+    global colour
+    run = True
+    click = False
+
+    bt_1 = pygame.Rect(100, 300, 100, 40)
+    bt_2 = pygame.Rect(250, 300, 100, 40)
+    bt_3 = pygame.Rect(400, 300, 100, 40)
+    bt_4 = pygame.Rect(550, 300, 100, 40)
+    bt_5 = pygame.Rect(700, 300, 100, 40)
+    bt_6 = pygame.Rect(100, 450, 100, 40)
+    bt_7 = pygame.Rect(250, 450, 100, 40)
+    bt_8 = pygame.Rect(400, 450, 100, 40)
+    bt_9 = pygame.Rect(550, 450, 100, 40)
+    bt_10 = pygame.Rect(700, 450, 100, 40)
+
+    def draw():
+        win.fill(colour)
+
+        if colour == BLACK:
+            draw_text('Options Menu', font, WHITE, win, 20, 20)
+            draw_text('Pick your background colour!', font, WHITE, win, 80, 80)
+        else:
+            draw_text('Options Menu', font, BLACK, win, 20, 20)
+            draw_text('Pick your background colour!', font, BLACK, win, 80, 80)
+
+        pygame.draw.rect(win, BLACK, bt_1)
+        pygame.draw.rect(win, WHITE, bt_2)
+        pygame.draw.rect(win, YLW, bt_3)
+        pygame.draw.rect(win, BBY_GRN, bt_4)
+        pygame.draw.rect(win, TEAL, bt_5)
+        pygame.draw.rect(win, GNTL_RED, bt_6)
+        pygame.draw.rect(win, PURPLE, bt_7)
+        pygame.draw.rect(win, RED, bt_8)
+        pygame.draw.rect(win, GREEN, bt_9)
+        pygame.draw.rect(win, BLUE, bt_10)
+
+    while run:
+        omx, omy = pygame.mouse.get_pos()
+
+        if bt_1.collidepoint(omx, omy):
+            if click:
+                colour = BLACK
+                draw()
+        if bt_2.collidepoint(omx, omy):
+            if click:
+                colour = WHITE
+                draw()
+        if bt_3.collidepoint(omx, omy):
+            if click:
+                colour = YLW
+                draw()
+        if bt_4.collidepoint(omx, omy):
+            if click:
+                colour = BBY_GRN
+                draw()
+        if bt_5.collidepoint(omx, omy):
+            if click:
+                colour = TEAL
+                draw()
+        if bt_6.collidepoint(omx, omy):
+            if click:
+                colour = GNTL_RED
+                draw()
+        if bt_7.collidepoint(omx, omy):
+            if click:
+                colour = PURPLE
+                draw()
+        if bt_8.collidepoint(omx, omy):
+            if click:
+                colour = RED
+                draw()
+        if bt_9.collidepoint(omx, omy):
+            if click:
+                colour = GREEN
+                draw()
+        if bt_10.collidepoint(omx, omy):
+            if click:
+                colour = BLUE
+                draw()
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                elif event.key == pygame.K_BACKSPACE:
+                    run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        draw()
         pygame.display.update()
         clock.tick(30)
 
